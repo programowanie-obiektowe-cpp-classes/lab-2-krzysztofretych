29a1c492224104a5c959cd1fa588b67b5105263c
krzysztofretych <krzysztof.retych@protonmail.com>
2022-11-01 20:48:58 +0100

    dalej nie dziala mi cmake

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,47 @@
 #pragma once
-
+#include<iostream>
 #include "Resource.hpp"
-
+using namespace std;
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    ResourceManager()
+    {
+        res=new Resource;
+    }
+    ~ResourceManager()
+    {
+        delete res;
+    }
+    ResourceManager(const ResourceManager& kopia)
+    {
+        res=new Resource(*kopia.res);
+    }
+    ResourceManager& operator=(const ResourceManager& xxx)
+    {
+        if (&xxx!=this)
+        {
+            delete res;
+            res=new Resource{*xxx.res};
+        }
+        return *this;
+    }
+    ResourceManager(ResourceManager&& yyy)
+    {
+        res=yyy.res;
+        yyy.res=nullptr;
+    }
+    ResourceManager& operator=(ResourceManager&& zzz)
+    {
+        delete res;
+        res=zzz.res;
+        zzz.res=nullptr;
+        return *this;
+    }
+    double get()
+    {
+        return res->get();
+    }
+private:
+    Resource* res=nullptr;
 };
